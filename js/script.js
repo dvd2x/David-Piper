818d69a1165e76e3fb347e5c3fb4e87c836dd36f
David Piper <black.god1930@gmail.com>
2019-04-20 18:35:28 -0400

    completed portfolio site

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  let scrollLink = $(".scroll");
+  //smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $("body, html").animate(
+      {
+        scrollTop: $(this.hash).offset().top
+      },
+      1000
+    );
+  });
+});
